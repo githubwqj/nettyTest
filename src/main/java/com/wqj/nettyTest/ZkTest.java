835fd57e9468aa9f1c21e3472eac63f0238d15fb
githubwqj <893453172@qq.com>
2018-03-03 16:22:50 +0800

    新建zkclient

--- src/main/java/com/wqj/nettyTest/ZkTest.java
+++ src/main/java/com/wqj/nettyTest/ZkTest.java
@@ -1,17 +1,21 @@
 package com.wqj.nettyTest;
 
 import org.I0Itec.zkclient.ZkClient;
+import org.apache.zookeeper.CreateMode;
 
 public class ZkTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ZkClient client=new ZkClient("192.168.8.144:2181");
+		ZkClient client=new ZkClient("192.168.8.144:2181",20);
 		//创建节点
 		if(client.exists("/wuqingjie")) {
-			client.createPersistent("/wuqingjie");
-			client.create("/wuqingjie/test", "test","");
+//			client.createPersistent("/wuqingjie");
+			client.create("/wuqingjie/test", "test",CreateMode.EPHEMERAL);
+			client.create("/wuqingjie/test2", "test2",CreateMode.EPHEMERAL);
+			client.create("/wuqingjie/test3", "test3",CreateMode.EPHEMERAL);
 		}
+		
 		//创建子节点
 //		client.createPersistent("/test/1","1");
 		
