4347a8ed8c8ae0666972cb9f6e45fcb6237e1430
githubwqj <893453172@qq.com>
2018-03-07 18:35:07 +0800

    11

--- src/main/java/com/wqj/mynetty/server/Server1.java
+++ src/main/java/com/wqj/mynetty/server/Server1.java
@@ -9,6 +9,10 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 
 public class Server1 {
 
@@ -20,26 +24,31 @@ public class Server1 {
 	
 	
 	public void startServer() throws Exception{
-		EventLoopGroup bossLoopGroup = new NioEventLoopGroup();
+		EventLoopGroup bossLoopGroup = new NioEventLoopGroup(1);
 		EventLoopGroup workLoopGroup = new NioEventLoopGroup(); 
 		try {
 			//server端引导类
 			ServerBootstrap serverBootstrap = new ServerBootstrap();
 			serverBootstrap.group(bossLoopGroup,workLoopGroup)//多线程处理
+			.option(ChannelOption.SO_BACKLOG, 1024)
 			.channel(NioServerSocketChannel.class)//指定通道类型为nioscoket类型
+			.handler(new LoggingHandler(LogLevel.INFO))
 			.childHandler(new ChannelInitializer<SocketChannel>() {
 				@Override
-				protected void initChannel(SocketChannel channel) throws Exception {
+				public void initChannel(SocketChannel channel) throws Exception {
 					// TODO Auto-generated method stub
-					channel.pipeline().addLast(new ServerHandler1());
+					channel.pipeline()
+						.addLast(new LineBasedFrameDecoder(1024))//重点
+						.addLast(new StringDecoder()) //重点
+						.addLast(new ServerHandler1());
 				}
-			})
-			.option(ChannelOption.SO_BACKLOG, 128)
-			.childOption(ChannelOption.SO_KEEPALIVE, true);
+			});
+			
+//			.childOption(ChannelOption.SO_KEEPALIVE, true);
 		//最后绑定服务器等待直接绑定完成,调用sync()方法阻塞知道服务器完成绑定,然后等待通道关闭,sync是同步锁	
 		ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
 		System.out.println("开始监听端口"+channelFuture.channel());
-		channelFuture.channel().close().sync();
+		channelFuture.channel().closeFuture().sync();
 		System.out.println("已关闭通道:"+channelFuture.channel());
 		}finally {
 			bossLoopGroup.shutdownGracefully().sync();
@@ -49,6 +58,7 @@ public class Server1 {
 	}
 	
 	public static void main(String[] args) throws Exception {
+		
 		new Server1(8080).startServer();
 	}
 }
