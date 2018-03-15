04750d76498099e0d9db0cc5b37b45a6d4dfe0e0
githubwqj2 <2812930196@qq.com>
2018-03-16 00:45:56 +0800

    1

--- src/main/java/com/wqj/zk/discover/ServiceDiscovery.java
+++ src/main/java/com/wqj/zk/discover/ServiceDiscovery.java
@@ -6,6 +6,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 import com.wqj.constant.BaseConstant;
@@ -13,61 +14,64 @@ import com.wqj.constant.BaseConstant;
 public class ServiceDiscovery {
 	private CountDownLatch latch = new CountDownLatch(1);
 	private volatile List<String> dataList = new ArrayList<String>();
-	private String registryAddress;    
+	private String registryAddress;
 
 	public ServiceDiscovery(String registryAddress) {
-        this.registryAddress = registryAddress;
-        ZooKeeper zk = connectServer();
-        if (zk != null) {
-            watchNode(zk);
-        }
-    }    
+		this.registryAddress = registryAddress;
+		ZooKeeper zk = connectServer();
+		if (zk != null) {
+			watchNode(zk);
+		}
+	}
 
 	public String discover() {
-        String data = null;
-        int size = dataList.size();
-        if (size > 0) {
-            if (size == 1) {
-                data = dataList.get(0);
-            } else {
-                data = dataList.get(ThreadLocalRandom.current().nextInt(size));
-            }
-        }
-        return data;
-    }     
+		String data = null;
+		int size = dataList.size();
+		if (size > 0) {
+			if (size == 1) {
+				data = dataList.get(0);
+			} else {
+				data = dataList.get(ThreadLocalRandom.current().nextInt(size));
+			}
+		}
+		return data;
+	}
 
 	private ZooKeeper connectServer() {
-        ZooKeeper zk = null;
-        try {
-            zk = new ZooKeeper(registryAddress, BaseConstant.ZK_SESSION_TIMEOUT, new Watcher() {
-                public void process(WatchedEvent event) {
-                    if (event.getState() == Event.KeeperState.SyncConnected) {
-                        latch.countDown();
-                    }
-                }
-            });
-            latch.await();
-        } catch (IOException | InterruptedException e) {
-        }
-        return zk;
-    }    
+		ZooKeeper zk = null;
+		try {
+			zk = new ZooKeeper(registryAddress, BaseConstant.ZK_SESSION_TIMEOUT, new Watcher() {
+				public void process(WatchedEvent event) {
+					if (event.getState() == Event.KeeperState.SyncConnected) {
+						latch.countDown();
+					}
+				}
+			});
+			latch.await();
+		} catch (Exception e) {
+
+		}
+		return zk;
+	}
 
 	private void watchNode(final ZooKeeper zk) {
-        try {
-            List<String> nodeList = zk.getChildren(BaseConstant.ZK_REGISTRY_PATH, new Watcher() {
-                public void process(WatchedEvent event) {
-                    if (event.getType() == Event.EventType.NodeChildrenChanged) {
-                        watchNode(zk);
-                    }
-                }
-            });
-            List<String> dataList = new ArrayList<String>();
-            for (String node : nodeList) {
-                byte[] bytes = zk.getData(BaseConstant.ZK_REGISTRY_PATH + "/" + node, false, null);
-                dataList.add(new String(bytes));
-            }
-            this.dataList = dataList;
-        } catch (KeeperException | InterruptedException e) {
-        }
-    }
+		try {
+			List<String> nodeList = zk.getChildren(BaseConstant.ZK_REGISTRY_PATH, new Watcher() {
+				public void process(WatchedEvent event) {
+					if (event.getType() == Event.EventType.NodeChildrenChanged) {
+						watchNode(zk);
+					}
+				}
+			});
+			List<String> dataList = new ArrayList<String>();
+			for (String node : nodeList) {
+				byte[] bytes = zk.getData(BaseConstant.ZK_REGISTRY_PATH + "/" + node, false, null);
+				dataList.add(new String(bytes));
+			}
+			this.dataList = dataList;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
 }
