4943f760f1c47ccbdee451b2a72abb2a06c923ea
githubwqj2 <2812930196@qq.com>
2018-03-16 00:29:33 +0800

    2

---
+++ src/main/java/com/wqj/constant/BaseConstant.java
@@ -0,0 +1,14 @@
+package com.wqj.constant;
+
+/**
+ * @author wqj
+ *
+ */
+public class BaseConstant {
+	public static final String ZK_REGISTRY_PATH = "/wqj";
+	
+	public static final int ZK_SESSION_TIMEOUT = 2000;
+	
+	public static final String ZK_DATA_PATH = "/";
+	
+}
